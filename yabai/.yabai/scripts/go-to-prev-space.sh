5fea0dceef2d63b72586b1dca1873d1b7de6be11
Simone D'Avico <simone.davico@welld.ch>
2021-07-19 11:10:17 +0200

    chore: backup yabai, skhd and karabiner configs

---
+++ yabai/.yabai/scripts/go-to-prev-space.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current_space_in_display_index=$(yabai -m query --spaces --display | jq '[.[]|.focused > 0]|index(true)')
+
+spaces_in_display=$(yabai -m query --spaces --display | jq 'length')
+
+next_space_index=$((($current_space_in_display_index-1)%$spaces_in_display))
+
+next_space_id=$(yabai -m query --spaces --display | jq ".[$next_space_index]|.index")
+
+
+$(yabai -m space --focus "$next_space_id")
+
+
